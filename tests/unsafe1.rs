9ffb272d7b522df48db5dc84ed3511a34e159d42
songyahui <songyahuiapp@hotmail.com>
2021-12-22 16:51:25 +0800

    m

--- tests/unsafe1.rs
+++ tests/unsafe1.rs
@@ -7,12 +7,12 @@ fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
     let len = slice.len();
     let ptr = slice.as_mut_ptr();
 
-    //assert!(mid <= len);
+    assert!(mid <= len);
 
     unsafe {
         (
             slice::from_raw_parts_mut(ptr, mid),
-            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+            slice::from_raw_parts_mut(ptr.add(mid-1), len - mid),
         )
     }
 }
